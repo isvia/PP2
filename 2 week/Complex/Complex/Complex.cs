54685058891d3b5b1121fea34a168366ad33f122
Isa <isvia18081999@gmail.com>
2018-02-13 11:22:20 +0600

    Labs

---
+++ 2 week/Complex/Complex/Complex.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexClass
+{
+    class Complex
+    {
+        public int x, y;
+
+        public Complex() { }
+
+        public Complex(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex n = new Complex();
+            n.x = c1.x + c2.x;
+            n.y = c1.y + c2.y;
+            return n;
+        }
+
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+            Complex m = new Complex();
+            m.x = c1.x - c2.x;
+            m.y = c1.y - c2.y;
+            return m;
+        }
+
+        public override string ToString()
+        {
+            return x + " " + y;
+        }
+
+
+    }
+}
---
+++ 4 week/Serialization/Serialization/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    // класс и его члены объявлены как public
+    [Serializable]
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // стандартный конструктор без параметров
+        public Person()
+        { }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // объект для сериализации
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            string text = Console.ReadLine();
+            int age = int.Parse(text);
+            Person person = new Person(name, age);
+            Console.WriteLine("Object created");
+
+            // передаем в конструктор тип класса
+            XmlSerializer formatter = new XmlSerializer(typeof(Person));
+
+            // получаем поток, куда будем записывать сериализованный объект
+            using (FileStream fs = new FileStream("persons.xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, person);
+
+                Console.WriteLine("Object serialized");
+            }
+
+            // десериализация
+            using (FileStream fs = new FileStream("persons.xml", FileMode.OpenOrCreate))
+            {
+                Person newPerson = (Person)formatter.Deserialize(fs);
+
+                Console.WriteLine("Object deserialized");
+                Console.WriteLine("Name: {0} --- Age: {1}", newPerson.Name, newPerson.Age);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
